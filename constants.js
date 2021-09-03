5ab7f163e6704889803f762436cfaf625ab6ad53
Thomasbeso <89991936+Thomasbeso@users.noreply.github.com>
2021-09-03 13:52:19 +0100

    Update constants.js

--- constants.js
+++ constants.js
@@ -3,3 +3,15 @@ const MAX_NUMBER_OF_ROWS_AND_COLUMNS = 5
 
 const ASCII_CODE_OF_a = 97
 const ASCII_CODE_OF_A = 65
+
+const COL_PLAYER_COLOR = "rgb(252, 212, 212)"
+const ROW_PLAYER_COLOR = "rgb(213, 229, 245)"
+// use https://meyerweb.com/eric/tools/color-blend/#::1:rgbd
+const BLEND_COLOR = "rgb(233,221,229)"
+
+//Sconst GREY_1 = "WhiteSmoke"
+const GREY_1 = "Gainsboro"
+const GREY_2 = "LightGray"
+const GREY_3 = "Silver"
+const GREY_4 = "DarkGray"
+const GREY_5 = "Gray"
